e999f8eea9af2052936e87dd14bd9f0a8a454894
divy suthar <divysuthar029@gmail.com>
2024-08-24 20:55:21 +0530

    Python code add

--- Employee.py
+++ Employee.py
@@ -29,13 +29,13 @@ class Employee(ABC):
     
 class FullTimeEmployee(Employee):
     def __init__(self, name, id, monthlySalary):
-        super.__init__(name, id)
+        super().__init__(name, id)
         self.__monthlySalary = monthlySalary
         self.__bonus = 0
         self.__deduction = 0
         
     def calculateSalary(self):
-        return self.__monthlySalary + self.__bonus - self.__deductions
+        return self.__monthlySalary + self.__bonus - self.__deduction
     
     def calculateBonus(self):
         return self.__bonus
@@ -58,7 +58,7 @@ class PartTimeEmployee(Employee):
         self.__deduction = 0
         
     def calculateSalary(self):
-        return (self.__hoursWorked * self.__hourlyRate) + self.__bonus - self.__deductions
+        return (self.__hoursWorked * self.__hourlyRate) + self.__bonus - self.__deduction
     
     def calculateBonus(self):
         return self.__bonus
@@ -82,7 +82,8 @@ class PayrollSystem:
     def removeEmployee(self, id):
         employee_to_remove = None
         for employee in self.__employeeList:
-            if(employee.getid() == id):
+            if(employee.getId() == id):
+                print(employee.getId())
                 employee_to_remove = employee
                 break
         if(employee_to_remove != None):
@@ -90,9 +91,69 @@ class PayrollSystem:
         
     def displayEmployees(self):
         for employee in self.__employeeList:
-            print(f"Employee: {employee.getName()}")
-            print(f"Salary: {employee.calculateSalary()}")
-            print(f"Bonus: {employee.calculateBonus()}")
-            print(f"Deductions: {employee.calculateDeductions()}")
-            print("---")
+            print(f"  Employee: {employee.getName()}")
+            print(f"  Salary: {employee.calculateSalary()}")
+            print(f"  Bonus: {employee.calculateBonus()}")
+            print(f"  Deductions: {employee.calculateDeductions()}")
+            print("")
+            print("------------------------------")
+        print("---------   End   ------------")
+def main():
+    payroll_system = PayrollSystem()
+    
+    while True:
+        print("")
+        print("      Please Enter your choice for menu  ")
+        print("")
+        print(" Enter 1 -> Add Employee")
+        print(" Enter 2 -> Remove Employee")
+        print(" Enter 3 -> Display Employee")
+        print(" Enter 4 -> Exit")
+        print("")
+        
+        try:
+            choice = int(input("Enter your choice: "))
+            print("")
+            if choice == 1:
+                name = input("Enter employee name: ")
+                id = input("Enter employee id: ")
+                print("")
+                try:
+                    empType = int(input("Enter 1 for Full-Time Employee and 2 for Part-Time Employee: "))
+                    if empType == 1:
+                        monthlySalary = int(input("Enter monthly salary: "))
+                        fulltimeEmployee = FullTimeEmployee(name, id, monthlySalary)
+                        bonus = int(input(f"Enter bonus for {fulltimeEmployee.getName()}: "))
+                        deduction = int(input(f"Enter deduction for {fulltimeEmployee.getName()}: "))
+                        fulltimeEmployee.setBonus(bonus)
+                        fulltimeEmployee.setDeduction(deduction)
+                        payroll_system.addEmployee(fulltimeEmployee)
+                    elif empType == 2:
+                        hoursWorked = int(input("Enter hours worked: "))
+                        hourlyRate = int(input("Enter hourly rate: "))
+                        parttimeEmployee = PartTimeEmployee(name, id, hoursWorked, hourlyRate)
+                        bonus = int(input(f"Enter bonus for {parttimeEmployee.getName()}: "))
+                        deduction = int(input(f"Enter deduction for {parttimeEmployee.getName()}: "))
+                        parttimeEmployee.setBonus(bonus)
+                        parttimeEmployee.setDeduction(deduction)
+                        payroll_system.addEmployee(parttimeEmployee)
+                except ValueError:
+                    print("Invalid Choice.")
+            elif choice == 2:
+                empIdToRemove = input("Enter employee ID to remove: ")
+                payroll_system.removeEmployee(empIdToRemove)
+                print("")
+            elif choice == 3:
+                print("------  Employee List  ------")
+                print("")
+                payroll_system.displayEmployees()
+                print("")
+            elif choice == 4:
+                break
+            else:
+                print("Invalid Choice. Please select a valid option.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
             
+if __name__ == "__main__":
+    main()
